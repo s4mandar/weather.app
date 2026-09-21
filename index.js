684e22b266fdf9319257ec9b819b75048881339c
s4mandar <samandarxudoyberdiyev3302@gmail.com>
2026-09-21 16:55:31 +0500

    1.1

---
+++ index.js
@@ -0,0 +1,47 @@
+const weatherForm = document.querySelector(".weatherForm")
+const weatherInput = document.querySelector("#weatherInput")
+const cardDisplay = document.querySelector(".card")
+
+const apiKey = "7f05789e9a5d62d25c60f6529ecb614c"
+
+weatherForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const city = weatherInput.value
+
+    if(city) {
+        try {
+            const weatherData = await getWeatherData(city)
+            showWeatherInfo(weatherData)
+        } catch (error) {
+            console.error(error)
+            showError(error)
+        }
+    } else {
+        showError("Please enter city name!")
+    }
+})
+
+async function getWeatherData(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    
+    const data = await fetch(apiUrl)
+
+    if(!data.ok) {
+        throw new Error("Shahar topilmadi!")
+    }
+    return data.json()
+}
+
+function showWeatherInfo(data) {
+
+}
+
+function showError(msg) {
+    cardDisplay.textContent = ""
+    const errorDisplay = document.createElement("p")
+    errorDisplay.textContent = msg
+    cardDisplay.style.display = "flex"
+
+    cardDisplay.append(errorDisplay)
+}
